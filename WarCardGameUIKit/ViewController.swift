4a963cedf59d50657270e286f394e15b2cdeb8ef
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-18 17:24:03 -0600

    - Complete.

---
+++ WarCardGameUIKit/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WarCardGameUIKit
+//
+//  Created by Fred Javalera on 4/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+}
+
